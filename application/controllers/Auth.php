99d795bf38982379d15c316904e349ee1dbd3d30
Baharudin Pratama <baharudinp21@gmail.com>
2022-03-15 16:28:54 +0700

    setup user login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,4 +7,30 @@ class Auth extends CI_Controller
     {
         $this->load->view('auth/login');
     }
+
+    public function process()
+    {
+        $post = $this->input->post(null, true);
+        if (isset($post['login'])) {
+            $this->load->model('user_model');
+            $query = $this->user_model->login($post);
+            if ($query->num_rows() > 0) {
+                $row = $query->row();
+                $params = [
+                    'id' => $row->id,
+                    'level' => $row->level,
+                ];
+                $this->session->set_userdata($params);
+                echo "<script>
+                    alert('Log in success');
+                    window.location='" . site_url('dashboard') . "';
+                </script>";
+            } else {
+                echo "<script>
+                    alert('Log in failed, wrong username / password');
+                    window.location='" . site_url('auth/login') . "';
+                </script>";
+            }
+        }
+    }
 }
